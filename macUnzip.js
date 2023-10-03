be9de0e4b4c163fe23f6a0ad46af82ee163b927a
chenyitao <chenyitao.c@bytedance.com>
2023-10-03 10:26:21 +0800

    feat: 测试mac给ffprobe二进制文件加执行权限

---
+++ macUnzip.js
@@ -0,0 +1,7 @@
+const AdmZip = require('adm-zip')
+
+//mac unzip ffmpeg
+const admzipFfprobe = new AdmZip('./assets/ffprobe-universal.zip')
+const admzipFfmpeg = new AdmZip('./assets/ffmpeg-universal.zip')
+admzipFfprobe.extractAllTo('./resources/ffmpeg-binaries')
+admzipFfmpeg.extractAllTo('./resources/ffmpeg-binaries')
---
+++ windowsUnzip.js
@@ -0,0 +1,7 @@
+const AdmZip = require('adm-zip')
+
+//windows unzip ffmpeg
+const admzipFfprobe = new AdmZip('./assets/ffprobe.exe.zip')
+const admzipFfmpeg = new AdmZip('./assets/ffmpeg.exe.zip')
+admzipFfprobe.extractAllTo('./resources/ffmpeg-binaries')
+admzipFfmpeg.extractAllTo('./resources/ffmpeg-binaries')
